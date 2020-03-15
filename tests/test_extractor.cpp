73cf41dac347d19a3f3b34b473c383d00697f859
Jean-Roland <jean.roland.gosse@gmail.com>
2020-03-15 15:38:25 +0100

    tests: add extractor unit test

---
+++ tests/test_extractor.cpp
@@ -0,0 +1,29 @@
+#include <gtest/gtest.h>
+#include <QFile>
+#include "codeextractor.h"
+#include "data/output_extractor.h"
+
+TEST(test_extractor, extraction) {
+    // Get model file data
+    QFile file("../tests/data/model_extractor.c"); // Note: Test should be run from build/ for this path to work
+
+    if(!file.open(QIODevice::ReadOnly)) {
+        FAIL() << " Couldn't open file: " + file.errorString().toStdString();
+    }
+    QTextStream file_content(&file);
+    CodeExtractor test_extractor;
+
+    // Check extraction and extraction status
+    ASSERT_FALSE(test_extractor.getExtractionComplete());
+    test_extractor.extractFromSourceFile(prot_name, &file_content, cmd_list);
+    ASSERT_TRUE(test_extractor.getExtractionComplete());
+
+    // Check output
+    ASSERT_TRUE(test_extractor.getUnknownIncludes() == unk_inc);
+    ASSERT_TRUE(test_extractor.getUnknownDefinitions() == unk_def);
+    ASSERT_TRUE(test_extractor.getUnknownPrivateFunctions() == unk_pri_fct);
+    ASSERT_TRUE(test_extractor.getUnknownPublicFunctions() == unk_pub_fct);
+    ASSERT_TRUE(test_extractor.getUnknownPublicFunctionsHeaders() == unk_pub_hdr);
+    ASSERT_TRUE(test_extractor.getDefaultCommandHandler() == def_cmd_hdl);
+    ASSERT_TRUE(test_extractor.getCommandFunctions() == cmd_fct);
+}
