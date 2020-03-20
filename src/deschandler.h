2aae3421671ce090de7a0c21f8bac715b7e1d4f4
Jean-Roland <jean.roland.gosse@gmail.com>
2020-03-20 15:58:47 +0100

    fix: add missing function arg name

--- src/deschandler.h
+++ src/deschandler.h
@@ -9,5 +9,5 @@ private:
 public:
     DescHandler();
     static void load_desc(QFile& file, QList<Command *>& cmdArray, QString& protocolName, QString& protocolId);
-    static bool save_desc(QString filename, QList<Command *>, QString protocolName, QString protocolId);
+    static bool save_desc(QString filename, QList<Command *> cmdArray, QString protocolName, QString protocolId);
 };
