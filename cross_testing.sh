caeee850719cdc40a0dc02eebd98dffc166f8cf3
Jean-Roland <jean-roland@anotherbrain.ai>
2023-01-27 17:26:33 +0100

    build: update utility scripts

--- cross_testing.sh
+++ cross_testing.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 
-# Run from LCSF_Generator directory with LCSF_C_Stack directory next to it
+# Run from LCSF_Generator root directory with LCSF_C_Stack directory next to it
 
 # Generate protcol with import
-./build/src/LCSF_Generator_CLI -l example/Test.json -a ../LCSF_C_Stack/src/protocol/Test_Main.c -b ../LCSF_C_Stack/b-tests/Test_Main.c
+./build/bin/lcsf_generator_cli -l example/Test.json -a ../LCSF_C_Stack/src/protocols_a/Test_Main.c -b ../LCSF_C_Stack/src/protocols_b/Test_Main.c
 
 # Run LCSF_C_Stack tests
-cd ../LCSF_C_Stack &&
-ceedling test:all &&
-cd ./b-tests &&
-./b-tests.sh
+cd ../LCSF_C_Stack/ &&
+cmake --build build/ --target run_tests
+cd -
